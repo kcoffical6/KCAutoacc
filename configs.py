36f58c7e2f7f9438874bbf47c4cf9e77cf0a8391
kcoffical6 <99400211+kcoffical6@users.noreply.github.com>
2024-02-26 23:13:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "1669750"))
     API_HASH = getenv("API_HASH", "0f53ee8c576281995d621194aec588d8")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6560312984:AAG7iIu0DnpO3jpwHRgQV3jxY1QTklAc-us")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6544672048:AAFq6jSYjEHGmb8rEWja_UztGb8Y3fw1zag")
     FSUB = getenv("FSUB", "KCHDMOVIES2023")
     CHID = int(getenv("CHID", "-1001914958042"))#update channel
     SUDO = list(map(int, getenv("SUDO", "718979130").split()))
